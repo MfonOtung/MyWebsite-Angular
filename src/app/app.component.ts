5db554b6e508c65b6bb98998568b2914c39b635c
MfonOtung <mfonotung@gmail.com>
2024-06-10 17:05:46 +0000

    Website Initiallisation - Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./_components/header/header.component";
+import { BannerComponent } from "./_components/banner/banner.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent]
 })
 export class AppComponent {
   title = 'MfonWebApp_a';
