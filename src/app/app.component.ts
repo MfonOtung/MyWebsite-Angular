8c7ac2d6c6fa7809814a4220cd42f3f2563600db
MfonOtung <mfonotung@gmail.com>
2024-06-11 11:37:20 +0000

    Addition of Experience Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { HeaderComponent } from "./_components/header/header.component";
 import { BannerComponent } from "./_components/banner/banner.component";
 import { AboutMeComponent } from "./_components/about-me/about-me.component";
 import { MySkillsComponent } from "./_components/my-skills/my-skills.component";
+import { ExperienceComponent } from "./_components/experience/experience.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent, AboutMeComponent, MySkillsComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent, AboutMeComponent, MySkillsComponent, ExperienceComponent]
 })
 export class AppComponent {
   title = 'MfonWebApp_a';
